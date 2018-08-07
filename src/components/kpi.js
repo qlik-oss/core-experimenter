4ade6ee5ddc2039ca5ba4dcf0bf82cbc11d2785e
Aiham Azmeh <aaz@qlik.com>
2018-08-07 13:51:36 +0200

    Fancy kpis (#29)
    * ++ kpi
    
    * ++ nicer KPIs !?
    
    * ++ lint

--- src/components/kpi.js
+++ src/components/kpi.js
@@ -1,31 +1,64 @@
 
 import { render, html } from '../../node_modules/lit-html/lib/lit-extended';
+import css from '../assets/circle.css';
 
 class KPI extends HTMLElement {
   constructor() {
     super();
     this.root = this.attachShadow({ mode: 'open' });
     this.dt = null;
-    this._title = 'No title';
-    this.formula = '';
     this._error = null;
-    this.data = null;
-    this.inputChangeDelegate = () => {};
+    this.data = 0;
+    this.inputChangeDelegate = () => { };
   }
 
   connectedCallback() {
   }
 
-  _frm(e) {
-    console.log(e.value);
-    this.inputChangeDelegate(e.value);
+  get title() {
+    return this.getAttribute('title') ? this.getAttribute('title') : 'no title';
+  }
+
+  set title(newValue) {
+    this.setAttribute('title', newValue);
+    this.invalidate();
+  }
+
+  static get observedAttributes() {
+    return ['title', 'formula', 'size', 'field'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      switch (name) {
+        case 'name':
+          this.title = newValue;
+          break;
+        case 'formula':
+          this.formula = newValue;
+          break;
+        case 'size':
+          this.size = newValue;
+          break;
+        case 'color':
+          this.color = newValue;
+          break;
+        case 'field':
+          this.field = newValue;
+          break;
+        default:
+          break;
+      }
+      this.invalidate();
+    }
+  }
+
+  _frm(el) {
+    this.inputChangeDelegate(el.innerText);
   }
 
   get data() {
-    if (this.dt != null && this.dt[0] && this.dt[0][0].qText) {
-      return this.dt[0][0].qText;
-    }
-    return 'empty';
+    return (this.dt != null && this.dt[0] && this.dt[0][0].qText) ? this.dt[0][0].qText : 0;
   }
 
   set data(val) {
@@ -33,22 +66,49 @@ class KPI extends HTMLElement {
     this.invalidate();
   }
 
-  get title() {
-    return this._title;
-  }
-
-  set title(val) {
-    this._title = val;
-    // this.invalidate();
-  }
-
   get formula() {
-    return this._formula;
+    return this.getAttribute('formula') ? this.getAttribute('formula') : '';
   }
 
-  set formula(val) {
-    this._formula = val;
-    // this.invalidate();
+  set formula(newValue) {
+    this.setAttribute('formula', newValue);
+    this.invalidate();
+  }
+
+  get field() {
+    return this.getAttribute('field') ? this.getAttribute('field') : '';
+  }
+
+  set field(newValue) {
+    this.setAttribute('field', newValue);
+    this.invalidate();
+  }
+
+  get size() {
+    return this.getAttribute('size') ? this.getAttribute('size') : 'default';
+  }
+
+  set size(newValue) {
+    this.setAttribute('size', newValue);
+    this.invalidate();
+  }
+
+  get color() {
+    return this.getAttribute('color') ? this.getAttribute('color') : 'default';
+  }
+
+  set color(newValue) {
+    this.setAttribute('color', newValue);
+    this.invalidate();
+  }
+
+  get theme() {
+    return this.getAttribute('theme') ? this.getAttribute('theme') : 'default';
+  }
+
+  set theme(newValue) {
+    this.setAttribute('theme', newValue);
+    this.invalidate();
   }
 
   get error() {
@@ -73,11 +133,41 @@ class KPI extends HTMLElement {
   template() {
     /* eslint-disable */
     return html`
+    <style>
+      :host{
+        border: 0;
+        font-size: 16px;
+        color: grey;
+        max-width: 180px;
+        text-align: center;
+      }
+      div[contenteditable="true"]{
+        border: 0;
+        border-bottom: 1px solid #e6e6e6;
+        font-size: 22px;
+        display: inline-block;
+        max-width: 100%;
+        height: 22px;
+        /* text-overflow: ellipsis; */
+        overflow: hidden;
+        white-space: nowrap;
+      }
+      h2{
+        margin: 10px 0px;
+      }
+      ${css}
+    </style>
     <div>
       <h2>${this.title}</h2>
-      <input style="width:98%" on-change="${(e) => { this._frm(e.target); }}" value="${this.formula}"></input><br>
-      <slot style="width:98%" name="title">${this.data}</slot><br>
-      <slot style="width:98%;color:red" name="error">${this._error}</slot>
+      <div class$="c100 p${parseInt(this.data)} ${this.size} ${this.theme} ${this.color} center">
+          <span>${parseInt(this.data)}%</span>
+          <div class="slice">
+              <div class="bar"></div>
+              <div class="fill"></div>
+          </div>
+      </div>
+      <div contenteditable="true" on-input="${(e) => { this._frm(e.target); }}">${this.formula}</div>
+
     </div>
     `;
     /* eslint-enable */
