dbd92bcad6ce4ecc2b4b82f4c44a7a384665cc97
Aiham Azmeh <aaz@qlik.com>
2018-08-17 10:05:30 +0200

    ++ mini cleaning + formating

--- src/components/kpi.js
+++ src/components/kpi.js
@@ -182,47 +182,25 @@ class KPI extends HTMLElement {
   template() {
     /* eslint-disable */
     return html`
-    <style>
-      :host{
-        border: 0;
-        font-size: 16px;
-        color: grey;
-        max-width: 180px;
-        text-align: center;
-      }
-      div[contenteditable="true"]{
-        border: 0;
-        border-bottom: 1px solid #e6e6e6;
-        font-size: 16px;
-        display: inline-block;
-        max-width: 180px;
-        overflow: hidden;
-      }
-      h2{
-        margin: 10px 0px;
-      }
-      ${cssCircle}
-      ${cssKPI}
-    </style>
-    <div>
-      <h2>${this.title}</h2>
-      <div class$="c100 p${parseInt(this.data)} ${this.size} ${this.theme} ${this.color} center">
-          <span>${parseFloat(this.data.toFixed(2))}%</span>
-          <div class="slice">
-              <div class="bar"></div>
-              <div class="fill"></div>
-          </div>
-      </div>
-      <div id="hej" class="textArea" contenteditable="true"
-      on-blur="${(e) => {
-      this._highlight(e.target)
-    }}"
-      on-focus="${(e) => {
-      this._lowlight(e.target)
-    }}"
-      on-input="${(e) => {
-      this._frm(e.target);
-    }}">${this.formula}</div>
+      <style>      
+        ${cssKPI}
+        ${cssCircle}
+      </style>
+      <div>
+        <h2>${this.title}</h2>
+        <div class$="c100 p${parseInt(this.data)} ${this.size} ${this.theme} ${this.color} center">
+            <span>${parseFloat(this.data.toFixed(2))}%</span>
+            <div class="slice">
+                <div class="bar"></div>
+                <div class="fill"></div>
+            </div>
+        </div>
+        <div id="hej" class="textArea" contenteditable="true" 
+            on-blur="${(e) => { this._highlight(e.target) }}" 
+            on-focus="${(e) => { this._lowlight(e.target) }}"
+            on-input="${(e) => { this._frm(e.target); }}">
+            ${this.formula}
+        </div>
       </div>
     `;
     /* eslint-enable */
