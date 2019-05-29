7b013e355419388e3b3568868a8592de94defdaf
Aiham Azmeh <aaz@qlik.com>
2019-05-29 12:51:33 +0200

    fix: string import

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import string from 'rollup-plugin-string';
+import { string } from 'rollup-plugin-string';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import copy from 'rollup-plugin-copy-glob';
